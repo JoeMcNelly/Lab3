7ef7f0425da13c0880e7fa454e2cfd3bf8da73cc
JoeMcNelly <joemcnelly1@gmail.com>
2015-03-26 22:27:49 -0400

    passed test4

--- Lab3/src/TestPackage/PrimeFactorFinder.java
+++ Lab3/src/TestPackage/PrimeFactorFinder.java
@@ -8,11 +8,12 @@ public class PrimeFactorFinder {
 	public static List<Integer> generateList(int i) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if (i == 2){
+		if (i % 2 == 0){
 			primes.add(2);
-		}else if(i==3){
-			primes.add(3);
+			i/=2;
 		}
+		if (i > 1)
+			primes.add(i);
 		return primes;
 	}
 
