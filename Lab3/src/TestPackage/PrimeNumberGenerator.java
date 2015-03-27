2cd5b89c0cbbc563ab96ee6f2ad02c8af86a6993
JoeMcNelly <joemcnelly1@gmail.com>
2015-03-26 22:49:54 -0400

    starting task 5, wrote first test case and code

---
+++ Lab3/src/TestPackage/PrimeNumberGenerator.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumberGenerator {
+
+	public List<Integer> generateList(int i) {
+		
+		return new ArrayList<Integer>();
+	}
+
+}
---
+++ Lab3/src/TestPackage/TestPrimeGenerator.java
@@ -0,0 +1,53 @@
+package TestPackage;
+
+import java.util.Arrays;
+import java.util.Collection;
+ 
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class TestPrimeGenerator {
+   private Integer inputNumber;
+   private List<Integer> expectedResult;
+   private PrimeNumberGenerator primeNumberGenerator;
+
+   @Before
+   public void initialize() {
+	   primeNumberGenerator = new PrimeNumberGenerator();
+   }
+
+   // Each parameter should be placed as an argument here
+   // Every time runner triggers, it will pass the arguments
+   // from parameters we defined in primeNumbers() method
+   public TestPrimeGenerator(Integer inputNumber, 
+      List<Integer> expectedResult) {
+      this.inputNumber = inputNumber;
+      this.expectedResult = expectedResult;
+   }
+
+   @Parameterized.Parameters
+   public static Collection<Object[]> primeNumbers() {
+      return Arrays.asList(new Object[][] {
+         { 2, list() },
+      });
+   }
+
+   // This test will run 4 times since we have 5 parameters defined
+   @Test
+   public void testPrimeNumberChecker() {
+      assertEquals(expectedResult, 
+    		  primeNumberGenerator.generateList(inputNumber));
+   }
+   
+   private static List<Integer> list(Integer...integers){
+		return Arrays.asList(integers);
+		
+	}
+}
